8e68429ef36ee00fa7636907f73a99161d2e1e08
Kevin Emamirad <keyone@gmail.com>
2018-01-16 00:12:41 -0500

    rename file

--- main.go
+++ main.go
@@ -16,15 +16,16 @@ func main() {
 	fmt.Println("*** Top 10 Gainers in the past 1 hour with MarketCap Non Null ***")
 	fmt.Println("*****************************************")
 
-	fmt.Printf("%s\t%s\t%s\t\t%s\t%s\n", "Rank", "Symbol", "Price", "change1h", "MarketCAP")
+	fmt.Printf("%s\t%s\t%s\t\t%s\t%s\t%s\n", "Rank", "Symbol", "Price", "change1h", "MarketCAP", "Updated")
 
 	for _, coin := range coin.ByDecreasingPercentChange1H(api.GetAllCoins(1500)) {
-		fmt.Printf("%d\t%s\t$ %.4f\t%.4f%%\t$ %.2f\n",
+		fmt.Printf("%d\t%s\t$ %.4f\t%.4f%%\t$ %.2f\t%s\n",
 			coin.Rank,
 			coin.Symbol,
 			coin.PriceUSD,
 			coin.PercentChange1H,
-			coin.MarketCapUSD)
+			coin.MarketCapUSD,
+			coin.LastUpdated.Format("Jan 2 15:04:05 MST"))
 	}
 
 	// elapsed timer
